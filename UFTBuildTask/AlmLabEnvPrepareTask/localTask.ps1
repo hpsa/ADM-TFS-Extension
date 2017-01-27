63df0cd9731c1a987182d90b235141fcaac646fb
ybobrik <yuri.bobrik@hpe.com>
2017-01-27 17:35:52 +0200

    Adding back end for AlmLabEnvPrep task Adding 10 parameters to Task UI Updating installation\UFT.zip with jar

---
+++ UFTBuildTask/AlmLabEnvPrepareTask/localTask.ps1
@@ -0,0 +1,97 @@
+#
+# localTask.ps1
+#
+
+param(
+	[string][Parameter(Mandatory=$true)] $varAlmserv,
+	[string][Parameter(Mandatory=$true)] $varUserName,
+	[string] $varPass,
+	[string][Parameter(Mandatory=$true)] $varDomain,
+	[string][Parameter(Mandatory=$true)] $varProject,
+	[string][Parameter(Mandatory=$true)] $varEnvId,
+
+	[string][Parameter(Mandatory=$true)] $javaHomeSelection,
+	[string][Parameter(Mandatory=$true)] $createNewNamed,
+	[string][Parameter(Mandatory=$false)] $assignMessage,
+	[string][Parameter(Mandatory=$false)] $jdkUserInputPath,
+	[string][Parameter(Mandatory=$false)] $varPathToJSON,
+	[string][Parameter(Mandatory=$true)] $paramOnlyFirst,
+
+	[string][Parameter(Mandatory=$false)] $AddParam1,
+	[string][Parameter(Mandatory=$false)] $paramType1,
+	[string][Parameter(Mandatory=$false)] $paramName1,
+	[string][Parameter(Mandatory=$false)] $paramValue1,
+
+	[string][Parameter(Mandatory=$false)] $AddParam2,
+	[string][Parameter(Mandatory=$false)] $paramType2,
+	[string][Parameter(Mandatory=$false)] $paramName2,
+	[string][Parameter(Mandatory=$false)] $paramValue2,
+
+	[string][Parameter(Mandatory=$false)] $AddParam3,
+	[string][Parameter(Mandatory=$false)] $paramType3,
+	[string][Parameter(Mandatory=$false)] $paramName3,
+	[string][Parameter(Mandatory=$false)] $paramValue3,
+
+	[string][Parameter(Mandatory=$false)] $AddParam4,
+	[string][Parameter(Mandatory=$false)] $paramType4,
+	[string][Parameter(Mandatory=$false)] $paramName4,
+	[string][Parameter(Mandatory=$false)] $paramValue4,
+
+	[string][Parameter(Mandatory=$false)] $AddParam5,
+	[string][Parameter(Mandatory=$false)] $paramType5,
+	[string][Parameter(Mandatory=$false)] $paramName5,
+	[string][Parameter(Mandatory=$false)] $paramValue5,
+
+	[string][Parameter(Mandatory=$false)] $AddParam6,
+	[string][Parameter(Mandatory=$false)] $paramType6,
+	[string][Parameter(Mandatory=$false)] $paramName6,
+	[string][Parameter(Mandatory=$false)] $paramValue6,
+
+	[string][Parameter(Mandatory=$false)] $AddParam7,
+	[string][Parameter(Mandatory=$false)] $paramType7,
+	[string][Parameter(Mandatory=$false)] $paramName7,
+	[string][Parameter(Mandatory=$false)] $paramValue7,
+
+	[string][Parameter(Mandatory=$false)] $AddParam8,
+	[string][Parameter(Mandatory=$false)] $paramType8,
+	[string][Parameter(Mandatory=$false)] $paramName8,
+	[string][Parameter(Mandatory=$false)] $paramValue8,
+
+	[string][Parameter(Mandatory=$false)] $AddParam9,
+	[string][Parameter(Mandatory=$false)] $paramType9,
+	[string][Parameter(Mandatory=$false)] $paramName9,
+	[string][Parameter(Mandatory=$false)] $paramValue9,
+
+	[string][Parameter(Mandatory=$false)] $AddParam10,
+	[string][Parameter(Mandatory=$false)] $paramType10,
+	[string][Parameter(Mandatory=$false)] $paramName10,
+	[string][Parameter(Mandatory=$false)] $paramValue10
+)
+
+$uftworkdir = $env:UFT_LAUNCHER
+
+$stdout = "$uftworkdir\temp_build.log"
+$stderr = "$uftworkdir\temp_error_build.log"
+$jar = """$uftworkdir\bin\hpe.application.automation.tfs.almrestrunner-1.0-jar-with-dependencies.jar"""
+
+$args = "-jar $jar lep  ""$varAlmserv"" ""$varUserName"" ""pass:$varPass"" ""$varDomain"" ""$varProject"" ""$varEnvId"" ""$javaHomeSelection"" ""$createNewNamed"" ""assign:$assignMessage"" ""$jdkUserInputPath"" ""jsonpath:$varPathToJSON"" ""$paramOnlyFirst"" ""partype1:$paramType1"" ""parname1:$paramName1"" ""parval1:$paramValue1"" ""partype2:$paramType2"" ""parname2:$paramName2"" ""parval2:$paramValue2"" ""partype3:$paramType3"" ""parname3:$paramName3"" ""parval3:$paramValue3"" ""partype4:$paramType4"" ""parname4:$paramName4"" ""parval4:$paramValue4"" ""partype5:$paramType5"" ""parname5:$paramName5"" ""parval5:$paramValue5"" ""partype6:$paramType6"" ""parname6:$paramName6"" ""parval6:$paramValue6"" ""partype7:$paramType7"" ""parname7:$paramName7"" ""parval7:$paramValue7"" ""partype8:$paramType8"" ""parname8:$paramName8"" ""parval8:$paramValue8"" ""partype9:$paramType9"" ""parname9:$paramName9"" ""parval9:$paramValue9"" ""partype10:$paramType10"" ""parname10:$paramName10"" ""parval10:$paramValue10"""
+
+echo $args
+
+$process = (Start-Process java -ArgumentList $args -RedirectStandardOutput $stdout -RedirectStandardError $stderr -PassThru -Wait)
+
+if ($process.ExitCode -ne 0)
+{
+	$content = [IO.File]::ReadAllText($stdout)
+	Write-Error ($content)
+	$content = [IO.File]::ReadAllText($stderr)
+	Write-Error ($content)
+}
+else
+{
+	Get-Content $stdout
+	Get-Content $stderr
+}
+
+Remove-Item $stdout
+Remove-Item $stderr
